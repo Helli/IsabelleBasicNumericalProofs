b75c46f6cf891c34beddbda6282d57ddb9ad31b1
Fabian Immler <immler@in.tum.de>
2015-10-09 09:56:29 +0200

    conversion to software Float.float; do not track generated file

--- Float_twosum.thy
+++ Float_twosum.thy
@@ -2,9 +2,35 @@ theory Float_twosum
 imports
   Complex_Main
   "~~/src/HOL/Library/Code_Target_Numeral"
+  "~~/src/HOL/Library/Float"
   "$AFP/IEEE_Floating_Point/RoundError"
   "$AFP/IEEE_Floating_Point/Code_Float"
 begin
+
+subsection \<open>Conversion to software floats\<close>
+
+code_printing
+code_module "ToManExp" \<rightharpoonup> (SML)
+  \<open>fun tomanexp x =
+  let
+    val {man = m, exp = e} = Real.toManExp x;
+    val p = Math.pow (2.0, 53.0);
+    val ms = m * p;
+    val mi = Real.floor ms;
+    val ei = op Int.- (e, 53);
+  in (mi, ei)
+  end\<close>
+
+definition tomanexp::"float \<Rightarrow> integer * integer" where "tomanexp x = undefined"
+code_printing constant "tomanexp :: float \<Rightarrow> integer * integer" \<rightharpoonup>
+  (SML) "tomanexp"
+declare tomanexp_def[code del]
+
+definition toFloat::"float \<Rightarrow> Float.float" where
+  "toFloat x = (let (m, e) = tomanexp x in Float.Float (int_of_integer m) (int_of_integer e))"
+
+value [code] "((Float.Float 123 (-13)) + (Float.Float 13 (-13))) * ((Float.Float 123 (-13)))"
+value [code] "toFloat (float_of 1 / float_of 1243313)"
 
 subsection \<open>ensure rounding: store variables\<close>
 
@@ -46,41 +72,37 @@ subsection \<open>Implementation\<close>
 fun twosum::"float * float \<Rightarrow> float *float"
   where "twosum (a, b) =
     (let
-      s = STORE (a + b);
+      s =  STORE (a + b);
       an = STORE (s - b);
       bn = STORE (s - an);
       da = STORE (a - an);
       db = STORE (b - bn);
-      t = STORE (da + db)
+      t =  STORE (da + db)
     in (s, t))"
 
 
 subsection \<open>Test Values\<close>
 
-definition a :: "float"
+
+context begin
+
+private definition a :: "float"
   where "a = float_of 33"
 
-definition b :: "float"
+private definition b :: "float"
   where "b = float_of 1 / float_of 1243313"
 
-definition r :: "float"
+private definition r :: "float"
   where "r = a+b"
 
-definition test_input :: "float*float"
+private definition test_input :: "float*float"
   where "test_input = (a, b)"
 
-definition test_result :: "float*float"
+private definition test_result :: "float*float"
   where "test_result = twosum test_input"
 
-definition s :: "float"
+private definition s :: "float"
   where "s = fst test_result"
-
-
-subsection \<open>Output\<close>
-
-definition hello_world::"unit \<Rightarrow> unit" where
-  "hello_world _ = (println (STR ''Starting 2sum example...''))"
-value [code] "hello_world ()"
 
 primrec test where "test () =
   (let
@@ -92,12 +114,32 @@ primrec test where "test () =
     _ = print (STR ''t = '') ; _ = println (string_of_float (snd test_result))
   in println (STR ''done''))"
 
+primrec test2 where "test2 () = 
+  (let
+    (s, t) = twosum (a, b);
+    (af, bf) = (toFloat a, toFloat b);
+    (sf, tf) = (toFloat s, toFloat t)
+  in (normfloat (af + bf), normfloat (sf + tf)))"
+
+value [code] "test2 ()"
+
+value [code] "r \<le> s \<and> r \<ge> s "
+
+end
+
+(* hide_const (open) a *)
+
+subsection \<open>Output\<close>
+
+definition hello_world::"unit \<Rightarrow> unit" where
+  "hello_world _ = (println (STR ''Starting 2sum example...''))"
+value [code] "hello_world ()"
+
 value [code] "test ()"
 
 export_code test in SML module_name Test
 export_code test in SML module_name Test file "test.sml"
 
 (*This equality test should be true:*)
-value [code] "r \<le> s \<and> r \<ge> s "
 
 end
