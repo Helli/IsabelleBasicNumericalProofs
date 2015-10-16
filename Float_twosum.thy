edfe97e927454eb962e7de6010af93b11970ce7a
Fabian Immler <immler@in.tum.de>
2015-10-16 17:17:24 +0200

    added setup for conversions: float_of_int, int_to_string

--- Float_twosum.thy
+++ Float_twosum.thy
@@ -7,6 +7,23 @@ imports
   "$AFP/IEEE_Floating_Point/Code_Float"
 begin
 
+subsection \<open>Conversion from int\<close>
+
+definition "float_of_int i = Float (real_of_int i)"
+
+context includes integer.lifting begin
+lift_definition float_of_integer::"integer \<Rightarrow> float" is float_of_int .
+end
+
+lemma float_of_int[code]:
+  "float_of_int i = float_of_integer (integer_of_int i)"
+  by (auto simp: float_of_integer_def)
+
+code_printing
+  constant "float_of_integer :: integer \<Rightarrow> float" \<rightharpoonup> (SML) "Real.fromInt"
+declare [[code drop: float_of_integer]]
+
+
 subsection \<open>Conversion to software floats\<close>
 
 code_printing
@@ -41,6 +58,28 @@ declare [[code drop: STORE]]
 
 subsection \<open>just for debugging: floats to strings and printing\<close>
 
+subsubsection \<open>integers to strings\<close>
+
+definition int_to_string::"int \<Rightarrow> String.literal"
+  where "int_to_string x = STR ''''"
+
+context includes integer.lifting begin
+
+lift_definition integer_to_string::"integer \<Rightarrow> String.literal"
+  is int_to_string .
+
+end
+
+lemma [code]: "int_to_string x = integer_to_string (integer_of_int x)"
+  by (simp add: integer_to_string_def)
+
+code_printing
+  constant "integer_to_string :: integer \<Rightarrow> String.literal" \<rightharpoonup> (SML) "IntInf.toString"
+declare [[code drop: integer_to_string]]
+
+
+subsection \<open>printing floats\<close>
+
 instantiation float::term_of
 begin
 definition term_of::"float \<Rightarrow> term" where "term_of x = undefined"
@@ -59,13 +98,24 @@ definition print::"String.literal \<Rightarrow> unit" where
 
 code_printing constant "print :: String.literal \<Rightarrow> unit" \<rightharpoonup>
   (SML) "TextIO.print"
-declare [[code drop: "string_of_float"]]
+declare [[code drop: "print"]]
 
 lemma [code]: "term_of_class.term_of (x::float) \<equiv> Code.abort (string_of_float x) (\<lambda>_. undefined)"
   by (rule term_of_anything)
 
 definition println::"String.literal \<Rightarrow> unit" where
   "println x = (let _ = print x in print (STR ''\<newline>''))"
+
+subsection \<open>printing Float.floats\<close>
+
+definition print_sw_float::"Float.float \<Rightarrow> unit" where
+  "print_sw_float x = (let
+      _ = print (int_to_string (Float.mantissa x));
+      _ = print (STR '' * 2 ^ '');
+      _ = println (int_to_string (Float.exponent x))
+     in
+       ())"
+
 
 subsection \<open>Implementation\<close>
 
@@ -125,7 +175,5 @@ value [code] "test ()"
 
 export_code test in SML module_name Test
 export_code test in SML module_name Test file "test.sml"
-
-(*This equality test should be true:*)
 
 end
