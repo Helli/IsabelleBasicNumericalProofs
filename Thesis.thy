5f50e7a0c8e1e3b4359665d216e50de751588b7a
Helli <efbfi@web.de>
2016-02-03 11:02:30 +0100

    adjust document preparation

--- Thesis.thy
+++ Thesis.thy
@@ -4,22 +4,19 @@ imports test_utils
 begin
 (*>*)
 
-section \<open>Einleitung hier\<close>
+section \<open>Introduction\<close>
 
 text \<open>
-Hallo ich bin generierter Text
-
-
-\<close>
-
-text_raw \<open>
-\begin{itemize}
-\item Punkt 1
-\item \[ \int_0^t \pi \]
-\end{itemize}
-\<close>
-
-text \<open>Ich bin ein theorem: @{thm float_of_int_def}.\<close>
+The transformation of numerical methods to use machine arithmetic is known to be error-prone.
+Therefore it is appropriate and important to exercise care to minimize errors.
+Most of the numerical methods implemented in Isabelle had already been proven to be correct
+beforehand in Mathematics. While most of these proofs work in a rather general setting, some
+require additional assumptions or preconditions that are not necessarily given in the context
+of real operational code, even if it uses the well-known IEEE standard of floating point
+arithmetic. As a related problem, this standard also introduces a lot of additional cases for
+operation results with its special values. In consequence, one might consider working with
+arbitrary precision formats that implement the entire number format using only the infinitely
+precise integer operations.\<close>
 
 (*<*)
 end
